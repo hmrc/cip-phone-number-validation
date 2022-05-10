0d49a349e95a67b4774e7f38e9d67aca8d95772e
brdgeth <brdgeth@users.noreply.github.com>
2022-05-10 11:14:03 +0100

    CAV-28 - Validate phone number API

--- test/uk/gov/hmrc/cipphonenumbervalidation/controllers/ValidatePhoneNumberControllerSpec.scala
+++ test/uk/gov/hmrc/cipphonenumbervalidation/controllers/ValidatePhoneNumberControllerSpec.scala
@@ -24,37 +24,28 @@ import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.http.{HeaderNames, Status}
-import play.api.inject.bind
-import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.libs.json.{JsObject, JsValue, Json}
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{contentAsJson, contentAsString, defaultAwaitTimeout, status}
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout, status, stubControllerComponents}
 import play.mvc.Http.{HttpVerbs, MimeTypes}
 import uk.gov.hmrc.cipphonenumbervalidation.constants.ApplicationConstants.{INVALID, VALID}
 import uk.gov.hmrc.cipphonenumbervalidation.service.PhoneNumberValidationService
 
 class ValidatePhoneNumberControllerSpec extends AnyWordSpec with Matchers with MockitoSugar with BeforeAndAfterEach {
 
-  val validatePhoneNumberEndpointUrl: String = "/customer-insight-platform/phone-number/validate-details"
   val expectedErrorMessage: String = "Enter a valid telephone number"
   val mockPhoneNumberValidationService: PhoneNumberValidationService = mock[PhoneNumberValidationService]
-  val app = new GuiceApplicationBuilder()
-    .overrides(bind[PhoneNumberValidationService].toInstance(mockPhoneNumberValidationService))
-    .build()
-  val validatePhoneNumberController = app.injector.instanceOf[ValidatePhoneNumberController]
 
-  val phoneNumberJson: JsObject = Json.obj("phoneNumber" -> "01292123456")
-  val getValidatePhoneNumberRequest: FakeRequest[JsValue] = FakeRequest(HttpVerbs.POST, validatePhoneNumberEndpointUrl)
-    .withHeaders(HeaderNames.CONTENT_TYPE -> MimeTypes.JSON)
-    .withBody[JsValue](phoneNumberJson)
+  val validatePhoneNumberController = new ValidatePhoneNumberController(stubControllerComponents(), mockPhoneNumberValidationService)
 
   override def afterEach() {
     reset(mockPhoneNumberValidationService)
   }
 
-  "POST on /customer-insight-platform/phone-number/validate-details" should {
+  "POST on /customer-insight-platform/phone-number/validate-format" should {
     "return a 200 (Ok) http response when phone number is valid" in {
-      when(mockPhoneNumberValidationService.validatePhoneNumber(anyString())) thenReturn VALID
+      val getValidatePhoneNumberRequest = buildRequest("01292123456")
+      when(mockPhoneNumberValidationService.validatePhoneNumber(anyString())).thenReturn(VALID)
 
       val actual = validatePhoneNumberController.validateFormat()(getValidatePhoneNumberRequest)
 
@@ -66,13 +57,12 @@ class ValidatePhoneNumberControllerSpec extends AnyWordSpec with Matchers with M
 
     "return a 400 (Bad Request) http response " +
       "when phone number passes simple validation and does not pass complex validation" in {
-      when(mockPhoneNumberValidationService.validatePhoneNumber(anyString())) thenReturn INVALID
+      val getValidatePhoneNumberRequest = buildRequest("01292123456")
+      when(mockPhoneNumberValidationService.validatePhoneNumber(anyString())).thenReturn(INVALID)
 
       val actual = validatePhoneNumberController.validateFormat()(getValidatePhoneNumberRequest)
 
       status(actual) shouldBe Status.BAD_REQUEST
-      val bodyJson: JsValue = contentAsJson(actual)
-      (bodyJson \ "message").as[String] shouldBe expectedErrorMessage
       verify(mockPhoneNumberValidationService).validatePhoneNumber(ArgumentMatchers.eq("01292123456"))
     }
 
@@ -82,19 +72,15 @@ class ValidatePhoneNumberControllerSpec extends AnyWordSpec with Matchers with M
       val actual = validatePhoneNumberController.validateFormat()(getValidatePhoneNumberRequestInvalidBlank)
 
       status(actual) shouldBe Status.BAD_REQUEST
-      val bodyJson: JsValue = contentAsJson(actual)
-      (bodyJson \ "message").as[String] shouldBe expectedErrorMessage
       verifyNoInteractions(mockPhoneNumberValidationService)
     }
 
-    "return a 400 (Bad Request) http response when phone number is less than minimum length of 6" in {
-      val getValidatePhoneNumberRequestInvalidMinLength = buildRequest("01292")
+    "return a 400 (Bad Request) http response when phone number is less than minimum length of 7" in {
+      val getValidatePhoneNumberRequestInvalidMinLength = buildRequest("012921")
 
       val actual = validatePhoneNumberController.validateFormat()(getValidatePhoneNumberRequestInvalidMinLength)
 
       status(actual) shouldBe Status.BAD_REQUEST
-      val bodyJson: JsValue = contentAsJson(actual)
-      (bodyJson \ "message").as[String] shouldBe expectedErrorMessage
       verifyNoInteractions(mockPhoneNumberValidationService)
     }
 
@@ -104,8 +90,6 @@ class ValidatePhoneNumberControllerSpec extends AnyWordSpec with Matchers with M
       val actual = validatePhoneNumberController.validateFormat()(getValidatePhoneNumberRequestInvalidMaxLength)
 
       status(actual) shouldBe Status.BAD_REQUEST
-      val bodyJson: JsValue = contentAsJson(actual)
-      (bodyJson \ "message").as[String] shouldBe expectedErrorMessage
       verifyNoInteractions(mockPhoneNumberValidationService)
     }
 
@@ -113,6 +97,7 @@ class ValidatePhoneNumberControllerSpec extends AnyWordSpec with Matchers with M
 
   private def buildRequest(input: String): FakeRequest[JsValue] = {
     val json: JsObject = buildPayload(input)
+    val validatePhoneNumberEndpointUrl: String = "/customer-insight-platform/phone-number/validate-format"
     val request = FakeRequest(HttpVerbs.POST, validatePhoneNumberEndpointUrl)
       .withHeaders(HeaderNames.CONTENT_TYPE -> MimeTypes.JSON)
       .withBody[JsValue](json)
